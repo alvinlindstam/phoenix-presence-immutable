e7db1c7e19f4f9601eed17d0f5c305af9bb7739b
Alvin Lindstam <alvin@kundo.se>
2017-03-19 10:13:24 +0100

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -86,50 +86,50 @@ const extractMetas = (comparedPresence, newPresence) => {
   return emptyMap.set('metas', newMetas)
 }
 
-export const syncState = function(oldState, newState, onJoin, onLeave) {
-    newState = Immutable.fromJS(newState)
+export const syncState = function (oldState, newState, onJoin, onLeave) {
+  newState = Immutable.fromJS(newState)
 
-    const newByDiff = newState.groupBy((value, key) => oldState.has(key) ? 'collision' : 'new')
-    const inCollisions = newByDiff.get('collision', emptyMap)
+  const newByDiff = newState.groupBy((value, key) => oldState.has(key) ? 'collision' : 'new')
+  const inCollisions = newByDiff.get('collision', emptyMap)
 
     // for all keys found in both oldState and newState, find the metas that are only in one of them
-    const onlyInOld = inCollisions.map((newPresence, key) => extractMetas(newPresence, oldState.get(key)))
-    const onlyInNew = inCollisions.map((newPresence, key) => extractMetas(oldState.get(key), newPresence))
+  const onlyInOld = inCollisions.map((newPresence, key) => extractMetas(newPresence, oldState.get(key)))
+  const onlyInNew = inCollisions.map((newPresence, key) => extractMetas(oldState.get(key), newPresence))
 
-    const allNewPresences = newByDiff.get('new', emptyMap)
-    const notFoundPresences = oldState.filterNot((_presence, key) => newState.has(key))
+  const allNewPresences = newByDiff.get('new', emptyMap)
+  const notFoundPresences = oldState.filterNot((_presence, key) => newState.has(key))
 
-    return syncDiff(oldState, {
-      joins: allNewPresences.merge(onlyInNew),
-      leaves: notFoundPresences.merge(onlyInOld)
-    }, onJoin, onLeave)
-  }
+  return syncDiff(oldState, {
+    joins: allNewPresences.merge(onlyInNew),
+    leaves: notFoundPresences.merge(onlyInOld)
+  }, onJoin, onLeave)
+}
 
 export const syncDiff = function (state, {joins, leaves}, onJoin, onLeave) {
-    const immutableJoins = isImmutable(joins) ? joins : Immutable.fromJS(joins)
-    const immutableLeaves = isImmutable(leaves) ? leaves : Immutable.fromJS(leaves)
+  const immutableJoins = isImmutable(joins) ? joins : Immutable.fromJS(joins)
+  const immutableLeaves = isImmutable(leaves) ? leaves : Immutable.fromJS(leaves)
 
-    state = immutableJoins.reduce((state, newPresence, key) => {
-      const currentPresence = state.get(key)
-      if (currentPresence) {
-        newPresence = newPresence.set('metas', currentPresence.get('metas').concat(newPresence.get('metas')))
-      }
-      if (onJoin) { onJoin(key, currentPresence, newPresence) }
-      return state.set(key, newPresence)
-    }, state)
+  state = immutableJoins.reduce((state, newPresence, key) => {
+    const currentPresence = state.get(key)
+    if (currentPresence) {
+      newPresence = newPresence.set('metas', currentPresence.get('metas').concat(newPresence.get('metas')))
+    }
+    if (onJoin) { onJoin(key, currentPresence, newPresence) }
+    return state.set(key, newPresence)
+  }, state)
 
-    return immutableLeaves.reduce((state, leftPresence, key) => {
-      const currentPresence = state.get(key)
-      if (!currentPresence) { return state }
+  return immutableLeaves.reduce((state, leftPresence, key) => {
+    const currentPresence = state.get(key)
+    if (!currentPresence) { return state }
 
-      const refsToRemove = leftPresence.get('metas').map(m => m.get('phx_ref'))
+    const refsToRemove = leftPresence.get('metas').map(m => m.get('phx_ref'))
 
-      const currentMetas = currentPresence.get('metas').filterNot(p => refsToRemove.includes(p.get('phx_ref')))
-      const currentNewPresence = currentPresence.set('metas', currentMetas)
-      if (onLeave) { onLeave(key, currentNewPresence, leftPresence) }
-      return currentMetas.size ? state.set(key, currentNewPresence) : state.delete(key)
-    }, state)
-  }
+    const currentMetas = currentPresence.get('metas').filterNot(p => refsToRemove.includes(p.get('phx_ref')))
+    const currentNewPresence = currentPresence.set('metas', currentMetas)
+    if (onLeave) { onLeave(key, currentNewPresence, leftPresence) }
+    return currentMetas.size ? state.set(key, currentNewPresence) : state.delete(key)
+  }, state)
+}
 
 export const list = function (state, chooser = (key, presence) => presence) {
   return state.map((value, key) => {
